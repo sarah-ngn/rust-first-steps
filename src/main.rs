b58e5d3534a44a4db564f667c200222c5a22ccb3
unknown <sarah.nguyen@utt.fr>
2021-10-15 19:32:52 +0200

    Day 2 of learning Rust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,43 @@
-use crate::update_car::execute;
+use crate::handle_errors::execute;
 mod build_car;
 mod update_car;
+mod hashmap_car;
+mod handle_errors;
+
+use std::fs::File;
+use std::io::{Error, Read};
+use std::path::PathBuf;
+
+fn read_file_contents(path: PathBuf) -> Result<String, Error> {
+    let mut string = String::new();
+
+    // Access a file at a specified path
+    // ---------------------------------
+    // TODO #1:
+    // - Pass variable to `file` variable on success, or
+    // - Return from function early if there's an error
+    let mut file: File = match File::open(path) {
+        Ok(file_handle) => file_handle,
+        Err(io_error) => return Err(io_error)
+    };
+
+    // Read file contents into `String` variable with `read_to_string`
+    // ---------------------------------
+    // Success path is already filled in
+    // TODO #2: Return from function early if there's an error
+    match file.read_to_string(&mut string) {
+        Ok(_) => (),
+        Err(io_error) => return Err(io_error)
+    };
+
+    Ok(string)
+}
 
 fn main() {
-    execute()
+    if read_file_contents(PathBuf::from("src/main.rs")).is_ok() {
+        println!("The program found the main file.");
+    }
+    if read_file_contents(PathBuf::from("non-existent-file.txt")).is_err() {
+        println!("The program reported an error for the file that doesn't exist.");
+    }
 }
